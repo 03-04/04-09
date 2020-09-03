bf84a854407af11096f216638d38de184773ef26
03-04 <ab.nenad@outlook.com>
2020-09-03 21:51:06 +0200

    Minor fix

--- functions/service.js
+++ functions/service.js
@@ -1,9 +1,8 @@
 const https = require('https');
-const API_ENDPOINT = 'https://api.nasa.gov/planetary';
 
 exports.handler = (event, context, response) => {
     const date = `${event.queryStringParameters.year || '2020'}-09-04`;
-    const url = `${API_ENDPOINT}/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`;
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`;
 
     let body = "";
     https.get(url, res => {
@@ -14,10 +13,6 @@ exports.handler = (event, context, response) => {
         res.on("end", () => {
             response(null, {
                 statusCode: 200,
-                header: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET'
-                },
                 body: body
             });
         });
