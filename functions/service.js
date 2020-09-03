4243c268bdf2f4f9db12ba4ccfed44cc22667476
03-04 <ab.nenad@outlook.com>
2020-09-03 21:27:39 +0200

    Initial commit

---
+++ functions/service.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const API_ENDPOINT = 'https://api.nasa.gov/planetary';
+
+exports.handler = (event, context, response) => {
+    const date = `${event.queryStringParameters.year || '2020'}-09-04`;
+    const url = `${API_ENDPOINT}/apod?api_key=${process.env.NASA_API_KEY}&date=${date}`;
+    
+    let body = "";
+    https.get(url, res => {
+        res.on("data", data => {
+            body += data;
+        });
+       
+        res.on("end", () => {
+            response(null, {
+                statusCode: 200,
+                body: body
+            });
+        });
+    });
+};
